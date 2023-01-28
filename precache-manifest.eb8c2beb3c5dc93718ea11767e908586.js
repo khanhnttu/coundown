ae8852189fe3a494609ffbabb5c34aaea26b73fb
khanhnd <khanh.nttu0204@gmail.com>
2023-01-28 16:31:43 +0700

    Updates

---
+++ precache-manifest.eb8c2beb3c5dc93718ea11767e908586.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5d804e1254e6bff554c18af4a3d4bd3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "abe48a701c6cf03b3bb5",
+    "url": "./static/css/main.b693b373.chunk.css"
+  },
+  {
+    "revision": "694b25c0d661edd7c95f",
+    "url": "./static/js/2.5846b019.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.5846b019.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abe48a701c6cf03b3bb5",
+    "url": "./static/js/main.e20a6a04.chunk.js"
+  },
+  {
+    "revision": "0d483da105536c33dc18",
+    "url": "./static/js/runtime-main.377d3a04.js"
+  }
+]);
